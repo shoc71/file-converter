43d8102f85fb1352c5937beb09c5aaae088cad9f
shoc71 <supersonicwind6@gmail.com>
2025-12-23 04:59:47 -0500

    error page and routing

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ErrorComponent } from './pages/error/error.component';
+import { ImportComponent } from './pages/import/import.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: ImportComponent},
+
+    // error
+    {path: 'error', component: ErrorComponent},
+    {path: '**', redirectTo: 'error'}
+];
